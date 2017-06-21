1f7b88fb99bd77374e072cbfbafd3c193a7938d3
user <user@localhost>
2017-06-21 10:30:21 +0900

    drbm

---
+++ drbm_c_sharp/drbm_c_sharp/DRBM.cs
@@ -0,0 +1,548 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drbm_c_sharp
+{
+    class DRBM
+    {
+        public int xSize; // input node size
+        public int hSize; // hidden node size
+        public int ySize; // output node size
+
+        // variables
+        List<double> x;  // input nodes
+        List<double> h;  // hidden nodes
+        List<double> y;  // output nodes
+
+        // params
+        List<List<double>> w;  // coupling params between input and hidden    
+        List<double> c;  // hidden bias params
+        List<List<double>> v;  // cpupling params between hidden and output
+        List<double> d;  // output bias params
+
+        protected double _oneExp(int j, int k)
+        {
+            double val = 1.0 + Math.Exp(this._lambda(j, k));
+
+            return val;
+        }
+
+        protected double _prodOneExpNeqJ(int j, int k)
+        {
+            double val = 1.0;
+
+            for (int l = 0; l < j; l++)
+            {
+                val *= this._oneExp(j, k);
+            }
+
+            for (int l = j + 1; l < this.hSize; l++)
+            {
+                val *= this._oneExp(j, k);
+            }
+
+            return val;
+        }
+
+        protected double _exp(int j, int k)
+        {
+            return Math.Exp(this._lambda(j, k));
+        }
+
+        protected double _sigmoid(ref List<double> data, int label_no, int j)
+        {
+            double lambda = 0.0;
+
+            for (int i = 0; i < this.xSize; i++)
+            {
+                lambda += this.w[i][j] * data[i];
+            }
+
+            lambda += this.c[j];
+            lambda += this.v[j][label_no];
+
+            return 1.0 / (1.0 + Math.Exp(-lambda));
+        }
+
+        protected double _lambda(int j, int k)
+        {
+            double val = 0.0;
+
+            for (int i = 0; i < this.xSize; i++)
+            {
+                val += this.w[i][j] * this.x[i];
+            }
+
+            val += this.c[j];
+            val += this.v[j][k];
+
+            return val;
+        }
+
+        protected List<List<double>> _calcProdOneExpAndExp(ref List<double> data)
+        {
+            this.x = data;
+
+            List<List<double>> prod_table = new List<List<double>>(this.hSize);
+            for (int i = 0; i < this.hSize; i++)
+            {
+                prod_table[i] = new List<double>(this.ySize);
+                for (int j = 0; j < ySize; j++)
+                {
+                    prod_table[i][j] = 1.0;
+                }
+            }
+
+            for (int j = 0; j < this.hSize; j++)
+            {
+                for (int k = 0; k < this.ySize; k++)
+                {
+
+                    prod_table[0][k] *= this._oneExp(j, k);
+                }
+            }
+
+            for (int j = this.hSize - 1; 0 <= j; j--)
+            {
+                for (int k = this.ySize - 1; 0 <= k; k--)
+                {
+                    prod_table[j][k] = prod_table[0][k];
+                    prod_table[j][k] /= this._oneExp(j, k);
+                    prod_table[j][k] *= this._exp(j, k);
+                }
+            }
+
+            return prod_table;
+        }
+
+        protected List<List<double>> _calcExpectedXH(ref List<double> data)
+        {
+            this.x = data;
+
+            double norm_const = this.normalizeConstant();
+
+            List<List<double>> exp_values = new List<List<double>>(this.xSize);
+
+            for (int i = 0; i < this.xSize; i++)
+            {
+                exp_values[i] = new List<double>(this.hSize);
+                for (int j = 0; j < this.hSize; j++)
+                {
+                    exp_values[i][j] = 1.0 / norm_const;
+                }
+            }
+
+            // prod_j 2cosh pre calc
+            List<List<double>> prod_table = this._calcProdOneExpAndExp(ref data);
+
+            for (int i = 0; i < this.xSize; i++)
+            {
+                for (int j = 0; j < this.hSize; j++)
+                {
+                    double sum_k = 0.0;
+
+                    for (int k = 0; k < this.ySize; k++)
+                    {
+                        sum_k += prod_table[j][k] * Math.Exp(this.d[k]);
+                    }
+
+                    exp_values[i][j] *= sum_k * data[i];
+                }
+            }
+
+            return exp_values;
+        }
+
+        protected List<double> _calcExpectedH(ref List<double> data)
+        {
+            this.x = data;
+
+            double norm_const = this.normalizeConstant();
+            List<double> exp_values = new List<double>(this.hSize);
+
+            for (int i = 0; i < this.hSize; i++)
+            {
+                exp_values[i] = 1.0 / norm_const;
+            }
+
+            // prod_j 2cosh pre calc
+            List<List<double>> prod_table = this._calcProdOneExpAndExp(ref data);
+
+            for (int j = 0; j < this.hSize; j++)
+            {
+                double tmp_val = 0.0;
+
+                for (int k = 0; k < this.ySize; k++)
+                {
+                    tmp_val += Math.Exp(this.d[k]) * prod_table[j][k];
+                }
+
+                exp_values[j] *= tmp_val;
+            }
+
+            return exp_values;
+        }
+
+        protected List<List<double>> _calcExpectedHY(ref List<double> data)
+        {
+            this.x = data;
+
+            double norm_const = this.normalizeConstant();
+
+            List<List<double>> exp_values = new List<List<double>>(this.hSize);
+
+            for (int i = 0; i < this.hSize; i++)
+            {
+                exp_values[i] = new List<double>(this.ySize);
+
+                for (int j = 0; j < this.ySize; j++)
+                {
+                    exp_values[i][j] = 1.0 / norm_const;
+                }
+            }
+
+            // prod_j 2cosh pre calc
+            List<List<double>> prod_table = this._calcProdOneExpAndExp(ref data);
+
+            for (int j = 0; j < this.hSize; j++)
+            {
+                for (int k = 0; k < this.ySize; k++)
+                {
+                    exp_values[j][k] *= Math.Exp(this.d[k]) * prod_table[j][k];
+
+                }
+
+            }
+
+            return exp_values;
+
+        }
+
+        protected List<double> _calcExpectedY(ref List<double> data)
+        {
+            this.x = data;
+            double norm_const = this.normalizeConstant();
+
+            List<double> exp_vals = new List<double>(this.ySize);
+            for (int i = 0; i < this.ySize; i++)
+            {
+                exp_vals[i] = 1.0 / norm_const;
+            }
+
+            for (int k = 0; k < this.ySize; k++)
+            {
+
+                exp_vals[k] *= Math.Exp(this.d[k]);
+
+                for (int j = 0; j < this.hSize; j++)
+                {
+                    exp_vals[k] *= this._oneExp(j, k);
+                }
+            }
+
+            return exp_vals;
+        }
+
+        protected double _uniform()
+        {
+            // FIXME: Unityで[0, 1]の一様乱数を生成する処理を実装してください
+            System.Random rand = new System.Random(100);
+            return rand.NextDouble();
+        }
+
+        protected void _shuffle<T>(ref List<T> array)
+        {
+            System.Random rand = new System.Random(100);
+
+            int n = array.Count;
+            T tmp;
+            for (int i = 0; i < n; i++)
+            {
+                int r = i + (int)(rand.NextDouble() * (n - i));
+                tmp = array[r];
+                array[r] = array[i];
+                array[i] = tmp;
+            }
+        }
+
+        public DRBM(int x_size, int h_size, int y_size)
+        {
+            this.xSize = x_size;
+            this.hSize = h_size;
+            this.ySize = y_size;
+
+            // constracting vars & params
+            this.x = new List<double>(x_size);
+            this.h = new List<double>(h_size);
+            this.y = new List<double>(y_size);
+
+            this.w = new List<List<double>>(x_size);
+            for (int i = 0; i < this.xSize; i++)
+            {
+                this.w[i] = new List<double>(h_size);
+            }
+
+            this.c = new List<double>(h_size);
+
+            this.v = new List<List<double>>(h_size);
+            for (int i = 0; i < this.hSize; i++)
+            {
+                v[i] = new List<double>(y_size);
+            }
+
+            this.d = new List<double>(y_size);
+
+            // initialize params
+            for (int i = 0; i < this.xSize; i++)
+            {
+                for (int j = 0; j < this.hSize; j++)
+                {
+                    this.w[i][j] = this._uniform();
+                }
+            }
+
+            for (int i = 0; i < this.hSize; i++)
+            {
+                this.c[i] = this._uniform();
+            }
+
+            for (int i = 0; i < this.hSize; i++)
+            {
+                for (int j = 0; j < this.ySize; j++)
+                {
+                    this.v[i][j] = this._uniform();
+                }
+            }
+
+            for (int i = 0; i < this.ySize; i++)
+            {
+                this.d[i] = this._uniform();
+            }
+        }
+
+
+        public double normalizeConstant()
+        {
+            // FIXME: what is x?
+            double norm_const = 0.0;
+            // y loop
+            Func<int, double> exp_prod_one_plus_exp =
+                (k) =>
+                {
+                    double constant = Math.Exp(this.d[k]);
+
+                    for (int j = 0; j < this.hSize; j++)
+                    {
+                        constant *= this._oneExp(j, k);
+                    }
+
+                    return constant;
+                };
+
+            for (int k = 0; k < this.ySize; k++)
+            {
+                norm_const += exp_prod_one_plus_exp(k);
+            }
+
+            return norm_const;
+        }
+
+        public int discriminate(ref List<double> data)
+        {
+            this.x = data;
+
+            // calc class prob;
+            Func<int, double, double> class_prob =
+                (k, norm_constnce) =>
+                {
+                    double potential = Math.Exp(this.d[k]) / norm_constnce;
+
+                    for (int j = 0; j < this.hSize; j++)
+                    {
+                        potential *= this._oneExp(j, k);
+                    }
+
+                    return potential;
+                };
+
+            double norm_const = this.normalizeConstant();
+
+            // discriminate
+            List<double> probs = new List<double>(this.ySize);
+
+            for (int k = 0; k < this.ySize; k++)
+            {
+                probs[k] = class_prob(k, norm_const);
+            }
+
+            // what is max-prob?
+
+            var max_elem = probs.Max();
+            int class_no = probs.IndexOf(max_elem);
+
+            return class_no;
+        }
+
+
+        public void train(ref List<List<double>> dataset, ref List<int> labelset, int batch_size, double learning_rate, int epoch)
+        {
+
+            List<List<double>> mom_xh = new List<List<double>>(this.xSize);
+            for (int i = 0; i < this.xSize; i++)
+            {
+                mom_xh[i] = new List<double>(this.hSize);
+            }
+
+            List<double> mom_h = new List<double>(this.hSize);
+
+            List<List<double>> mom_hy = new List<List<double>>(this.hSize);
+            for (int i = 0; i < this.ySize; i++)
+            {
+                mom_hy[i] = new List<double>(this.ySize);
+            }
+
+            List<double> mom_y = new List<double>(this.ySize);
+
+            List<int> indexes = new List<int>(labelset.Count);
+
+            for (int i = 0; i < indexes.Count; i++) indexes[i] = i;
+
+            for (int e = 0; e < epoch; e++)
+            {
+                List<List<double>> xh_data = new List<List<double>>(this.xSize);
+                for (int i = 0; i < this.xSize; i++)
+                {
+                    xh_data[i] = new List<double>(this.hSize);
+                }
+
+                List<double> h_data = new List<double>(this.hSize);
+
+                List<List<double>> hy_data = new List<List<double>>(this.hSize);
+                for (int i = 0; i < this.hSize; i++)
+                {
+                    hy_data[i] = new List<double>(this.ySize);
+                }
+
+                List<double> y_data = new List<double>(this.ySize);
+
+                List<List<double>> xh_drbm = new List<List<double>>(this.xSize);
+                for (int i = 0; i < this.xSize; i++)
+                {
+                    xh_drbm[i] = new List<double>(this.hSize);
+                }
+
+                List<double> h_drbm = new List<double>(this.hSize);
+
+                List<List<double>> hy_drbm = new List<List<double>>(this.hSize);
+                for (int i = 0; i < this.hSize; i++)
+                {
+                    hy_drbm[i] = new List<double>(this.ySize);
+                }
+
+                List<double> y_drbm = new List<double>(this.ySize);
+
+
+
+                // shuffle index
+                this._shuffle(ref indexes);
+
+                for (int n = 0; n < batch_size; n++)
+                {
+                    // calc sample means
+                    int index = indexes[n];
+
+                    var data = dataset[index];
+                    var label = labelset[index];
+
+                    for (int j = 0; j < this.hSize; j++)
+                    {
+                        var sig_j = this._sigmoid(ref data, label, j) / (double)batch_size;
+
+                        for (int i = 0; i < this.xSize; i++)
+                        {
+                            xh_data[i][j] += data[i] * sig_j;
+                        }
+                    }
+
+                    for (int j = 0; j < this.hSize; j++)
+                    {
+                        h_data[j] += this._sigmoid(ref data, label, j) / (double)batch_size;
+                        hy_data[j][label] += this._sigmoid(ref data, label, j) / (double)batch_size;
+                    }
+
+                    y_data[label] += 1.0 / (double)batch_size;
+
+                    // calc DRBMSig expected values
+                    var exp_xh = this._calcExpectedXH(ref data);
+                    var exp_h = this._calcExpectedH(ref data);
+                    var exp_hy = this._calcExpectedHY(ref data);
+                    var exp_y = this._calcExpectedY(ref data);
+
+                    for (int i = 0; i < this.xSize; i++)
+                    {
+                        for (int j = 0; j < this.hSize; j++)
+                        {
+                            xh_drbm[i][j] += exp_xh[i][j] / (double)batch_size;
+                        }
+                    }
+
+                    for (int j = 0; j < this.hSize; j++)
+                    {
+                        h_drbm[j] += exp_h[j] / (double)batch_size;
+
+
+
+                        for (int k = 0; k < this.ySize; k++)
+                        {
+                            hy_drbm[j][k] += exp_hy[j][k] / (double)batch_size;
+                        }
+                    }
+
+                    for (int k = 0; k < this.ySize; k++)
+                    {
+                        y_drbm[k] += exp_y[k] / (double)batch_size;
+                    }
+                }
+
+                // update params
+                for (int i = 0; i < this.xSize; i++)
+                {
+                    for (int j = 0; j < this.hSize; j++)
+                    {
+                        var delta = 0.95 * mom_xh[i][j] + learning_rate * (xh_data[i][j] - xh_drbm[i][j]);
+
+                        mom_xh[i][j] = delta;
+                        this.w[i][j] += delta;
+                    }
+                }
+
+                for (int j = 0; j < this.hSize; j++)
+                {
+                    var delta = 0.95 * mom_h[j] + learning_rate * (h_data[j] - h_drbm[j]);
+
+                    mom_h[j] = delta;
+                    this.c[j] += delta;
+
+                    for (int k = 0; k < this.ySize; k++)
+                    {
+                        var delta2 = 0.95 * mom_hy[j][k] + learning_rate * (hy_data[j][k] - hy_drbm[j][k]);
+
+                        mom_hy[j][k] = delta2;
+                        this.v[j][k] += delta2;
+                    }
+                }
+
+                for (int k = 0; k < this.ySize; k++)
+                {
+                    var delta = 0.95 * mom_y[k] + learning_rate * (y_data[k] - y_drbm[k]);
+
+                    mom_y[k] = delta;
+                    this.d[k] += delta;
+                }
+            }
+        }
+    }
+}
---
+++ drbm_c_sharp/drbm_c_sharp/UnityJanken.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drbm_c_sharp
+{
+    class UnityJanken
+    {
+        public int historySize;  // 何手前まで考慮する?
+        public List<int> history;  // 相手の手の履歴 FIXME: さらに何件分まで訓練データとして使う?
+        public int tryCount = 0;  // 試行回数
+        public DRBM drbm;
+        public int hiddenSize = 50;
+        public int batchSize = 5;  // とりあえず1, データ少ないとき適宜対応
+        public double learningRate = 0.1;  // とりあえず0,1
+        public int epoch = 100;  // とりあえず100回
+
+
+        public UnityJanken(int histry_size)
+        {
+            this.historySize = histry_size;
+            this.history = new List<int>(histry_size);
+
+            // 可視変数: {不明, グー, チョキ, パー}^history_size -> 4 * history_size
+            // 隠れ変数: とりあえず50
+            // 出力は{グー, チョキ, パー}の3種類
+            this.drbm = new DRBM(4 * histry_size, 50, 3);
+        }
+
+        // class_no: 相手の出した手
+        public void train(int class_no)
+        {
+            // 相手の出し手に対して勝てる手は?
+            int win_no = this.getWinPattern(class_no);
+
+            // 現在の履歴で学習
+            // FIXME: 何件分のデータで学習するの?
+
+            // DRBM学習用データセット作成
+            List<List<double>> dataset;
+
+            // 勝てる手のラベルセット作成
+            List<List<double>> label;
+
+
+            this.drbm.train(ref dataset, ref label, this.batchSize, this.learningRate, this.epoch);
+        }
+        
+        // 入力に対して勝てる手を決める(後だしじゃんけん)
+        public int getWinPattern(int class_no)
+        {
+            // {グー, チョキ, パー} = {0, 1, 2}
+            // 勝つには{パー, グー, チョキ} = {2, 0, 1}
+            int[] pattern = new int[3] { 2, 0, 1 };
+
+            return pattern[class_no];
+        }
+
+        // 相手に勝てそうな手の予想
+        public int inference()
+        {
+            List<double> data = new List<double>(4 * this.historySize);  // DRBM入力用
+            // DRBMように可視変数設計
+
+            return drbm.discriminate(ref data);
+        }
+    }
+}
